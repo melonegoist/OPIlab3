48700c6c34c1a3c174e94786d79b2b25f2dc9a38
Melon Egoist <melon-egoist@mail.ru>
2025-05-29 01:42:44 +0300

    testing history task #3

--- src/main/java/ru/melon_egoist/auth/SecurityConfig.java
+++ src/main/java/ru/melon_egoist/auth/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@s
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
