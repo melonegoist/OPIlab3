4f083ae79a2cac02cb4a74718b4b90842290529f
Melon Egoist <melon-egoist@mail.ru>
2025-05-29 07:42:14 +0300

    added diff task version#3

--- src/main/java/ru/melon_egoist/Backend.java
+++ src/main/java/ru/melon_egoist/Backend.java
@@ -3,12 +3,6 @@ package ru.melon_egoist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// changed
-// changed 2
-// changed 3
-// changed 4
-// change 5
-
 @SpringBootApplication
 public class Backend {
     public static void main(String[] args) {
--- src/main/java/ru/melon_egoist/management/CorsConfig.java
+++ src/main/java/ru/melon_egoist/management/CorsConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// 1
 
 @Configuration
 public class CorsConfig {
