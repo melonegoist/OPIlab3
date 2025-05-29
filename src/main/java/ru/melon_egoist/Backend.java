233f465311018be1d233c4fcb4e083574c1a86df
Melon Egoist <melon-egoist@mail.ru>
2025-05-30 01:08:24 +0300

    commit

--- src/main/java/ru/melon_egoist/Backend.java
+++ src/main/java/ru/melon_egoist/Backend.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Backend {
-// testtt
     public static void main(String[] args) {
         SpringApplication.run(Backend.class, args);
     }
